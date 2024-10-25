145ace70cd70c6460d585700a5ecd9cc2a593869
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-10-25 11:41:27 +0600

    Add responsive banner in Header component using DaisyUI

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import bgShadow from '../../assets/bg-shadow.png';
+import bannerLogo from '../../assets/banner-main.png';
+
+const Banner = (() => {
+    return (
+        <div className="relative bg-black_color rounded-3xl overflow-hidden my-8">
+            {/* image layer over Background */}
+            <img className="absolute w-full h-full bg-cover z-0" src={bgShadow} alt="background shadow image" />
+
+            {/* Text content layer */}
+            <div className="relative z-10 flex flex-col items-center justify-center space-y-4 py-12">
+                <img src={bannerLogo} alt="banner logo image" />
+                <h1 className="text-3xl font-bold text-white_color text-center sm:w-4/5 md:w-full">Assemble Your Ultimate Dream 11 Cricket Team</h1>
+                <p className="font_inter text-xl font-medium text-shade_white_color_1 pb-4">Beyond Boundaries Beyond Limits</p>
+                <button className="btn text-sm font-bold bg-yellow_color outline outline-yellow_color outline-offset-8">Claim Free Credit</button>
+            </div>
+        </div>
+    );
+});
+
+Banner.propTypes = {
+
+};
+
+export default Banner;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 import logo from '../../assets/logo.png';
 import coin from '../../assets/coin.svg';
+import Banner from '../Banner/Banner';
 
 const Header = () => {
     return (
         <div>
             {/* navbar design */}
-            <div className="navbar sm:my-4 md:my-8">
+            <div className="navbar my-4">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabindex="0" role="button" className="btn btn-ghost md:hidden">
@@ -14,7 +15,7 @@ const Header = () => {
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
                             </svg>
                         </div>
-                        <ul tabindex="0" className="menu menu-sm dropdown-content rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        <ul tabindex="0" className="menu menu-sm dropdown-content rounded-box z-[1] mt-3 w-52 bg-white border border-grey-500">
                             <li>
                                 <a className='text-sm text-shade_black_color_1' href='#'>Home</a>
                             </li>
@@ -31,9 +32,10 @@ const Header = () => {
                     </div>
                     <img src={logo} alt="brand logo image" />
                 </div>
+
                 <div className="navbar-end">
                     <div className="hidden md:flex">
-                        <ul className="flex space-x-8 mr-8 px-1">
+                        <ul className="flex space-x-8 mr-8">
                             <li>
                                 <a className='text-sm text-shade_black_color_1' href='#'>Home</a>
                             </li>
@@ -54,6 +56,9 @@ const Header = () => {
                     </button>
                 </div>
             </div>
+
+            {/* banner design */}
+            <Banner></Banner>
         </div>
     );
 };
