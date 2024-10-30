c12e3618ff9c9f89e886b640cb08c5620b10d278
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-10-30 17:17:18 +0600

    Update index.html

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,12 +10,12 @@ const Header = ({ coins, handleToAddCoin, isActive }) => {
             <div className="navbar my-4">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <div tabindex="0" role="button" className="btn btn-ghost md:hidden">
-                            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
                             </svg>
                         </div>
-                        <ul tabindex="0" className="menu menu-sm dropdown-content rounded-box z-[1] mt-3 w-52 bg-white border border-grey-500">
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content rounded-box z-[1] mt-3 w-52 bg-white border border-grey-500">
                             <li>
                                 <a className='text-sm text-shade_black_color_1' href='#'>Home</a>
                             </li>
